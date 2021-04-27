666c09389318cfa1ddc8b98965f79310b05b82e7
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-04-27 11:02:15 +0300

    Завдання 2.3

---
+++ Завдання 2.3/Завдання 2.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int vvid;
+    int g;
+    int c;
+    cout << "Enter 2 numbers" << endl;
+    cin >> g;
+    cin >> c;
+    int a = g * c;
+    cout << "Enter your try" << endl;
+    cin >> vvid;
+    if (vvid == a)
+    {
+        cout << "Correct " << endl;
+    }
+    else
+    {
+        cout << "You're wrong. Correct anwser:\n" << a << endl;
+    }
+    return 0;
