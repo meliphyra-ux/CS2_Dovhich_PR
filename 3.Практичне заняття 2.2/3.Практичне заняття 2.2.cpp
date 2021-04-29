ab17c8aed12399d38e52fc098a5078703c2a12b8
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-04-29 14:03:45 +0300

    Проєктна робота

--- 3.Практичне заняття 2.2/3.Практичне заняття 2.2.cpp
+++ 3.Практичне заняття 2.2/3.Практичне заняття 2.2.cpp
@@ -12,17 +12,23 @@ int main()
 		n[i] = rand() % (100-1) + 1;
 		cout << "Massiv: " << n[i] << endl;
 	}
-	system("pause");
+	
+	int j=0;
+
 	for (int i = 0; i < m; i++)
 	{
+		
 		if (n[i] % 2 == 1)
 		{
-			cout << "Непарні: " << n[i] << endl;
+			
+			n[j] = n[i];
+			j++;
 		}
-		else
-		{
-
-		}
+		
 	
+	}
+	for (int i = 0; i < j; i++)
+	{
+		cout << "Massiv1:\n" << n[i] << endl;
 	}
 }
--- 3.Практичне заняття/3.Практичне заняття.cpp
+++ 3.Практичне заняття/3.Практичне заняття.cpp
@@ -13,12 +13,12 @@ int main()
 		cout << "Massiv: " << n[i] << endl;
 	}
 	
-	int max = n[0];//Помещаем значения 1-го элемента
-	int min = n[0];//массива в переменные
+	int max = n[0];
+	int min = n[0];
 	for (int r = 1; r < m; r++)
 	{
-		if (max < n[r]) max = n[r]; //если значение элемента больше значения переменной max, то записываем это значение в переменную
-		if (min > n[r]) min = n[r]; //аналогично и для min
+		if (max < n[r]) max = n[r];
+		if (min > n[r]) min = n[r];
 	}
 	cout << "Min: " << min << endl;
 	cout << "Max: " << max << endl;
