51dfc7f458e29823569886f70ff9ae06dfa16cf6
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-04-30 18:55:01 +0300

    Проєктна робота

--- 5. Практичне заняття/5. Практичнне заняття.cpp
+++ 5. Практичне заняття/5. Практичнне заняття.cpp
@@ -1,20 +1,44 @@
-﻿// 5. Практичнне заняття.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <string>
+
+class Car
+{
+private:
+    std::string m_company;
+    std::string m_model;
+
+public:
+    Car(std::string company, std::string model)
+        : m_company(company), m_model(model)
+    {
+    }
+
+    friend bool operator == (const Car& c1, const Car& c2);
+    friend bool operator != (const Car& c1, const Car& c2);
+};
+
+bool operator== (const Car& c1, const Car& c2)
+{
+    return (c1.m_company == c2.m_company &&
+        c1.m_model == c2.m_model);
+}
+
+bool operator!= (const Car& c1, const Car& c2)
+{
+    return !(c1 == c2);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Car mustang("Ford", "Mustang");
+    Car logan("Renault", "Logan");
+
+    if (mustang == logan)
+        std::cout << "Mustang and Logan are the same.\n";
+
+    if (mustang != logan)
+        std::cout << "Mustang and Logan are not the same.\n";
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
