e6a3c889b6ee2fb9ec447493bcefa3c84cb80b7c
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-05-07 12:38:43 +0300

    Проєктна робота

---
+++ 5.Практичне заняття 2.2/5.Практичне заняття 2.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int p=0;
+	int j = 1;
+	char word[80];
+
+	for (int i = 0; i < 80; i++)
+	{
+		cin >> word[i];
+		p++;
+		if (word[i] == '0')
+			i = 9;
+    }
+	for (int i = 0; i < p; i++)
+		cout << word[i] << endl;
+
+
+}
