2ed55a8257fad94c07a3919b7248b93e026dd33d
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-05-18 23:20:33 +0300

    Проєктна робота

---
+++ Індивідуальна робота №3/Індивідуальна робота №3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 10;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = (n - 1); j > i; j--) // для всех элементов после i-ого
+        {
+            if (a[j - 1] > a[j]) // если текущий элемент меньше предыдущего
+            {
+                int temp = a[j - 1]; // меняем их местами
+                a[j - 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << a[i] << endl;
+}
