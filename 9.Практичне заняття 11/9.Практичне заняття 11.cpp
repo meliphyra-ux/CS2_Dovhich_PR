05b1f0d2cc1257acc37208d769efd155f89b95c4
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-05-19 11:34:45 +0300

    Проєктна робота

--- 9.Практичне заняття 11/9.Практичне заняття 11.cpp
+++ 9.Практичне заняття 11/9.Практичне заняття 11.cpp
@@ -1,21 +1,29 @@
 ﻿#include <iostream>
 #include <cmath>
 using namespace std;
-
+double NCD(double num, int);
 int main()
 {
 	double integrer,fr,num;
+	int n = 10, i = 100;
+	int f;
 	cin >> num;
 	fr = modf(num, &integrer);
-	if (fr < 1,fr > 0.1)
+	if (fr < 1 && fr > 0.1)
 	{
 		num = (num * 10);
-		cout << num << "/10";
+		f = NCD(num, n);
+		num /= f;
+		n /= f;
+		cout << num << "/" << n << endl;
 	}
 	if (fr < 0.1)
 	{
 		num = (num * 100);
-		cout << num << "/100";
+		f = NCD(num, i);
+		num /= f;
+		i /= f;
+		cout << num << "/" << i << endl;
 	}
 	else
 	{
@@ -24,4 +32,17 @@ int main()
 	
 	return 0;
 
+}
+
+double NCD(double num, int n)
+{
+if (num == n) {
+	return num;
+}
+if (num > n) {
+	long tmp = num;
+	num = n;
+	n = tmp;
+}
+return NCD(num, n - num);
 }
