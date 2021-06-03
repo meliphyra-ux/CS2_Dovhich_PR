9f1dc566253a08ef38c1aaa8a0e365497dd8c994
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2021-06-03 11:12:40 +0300

    Борг

---
+++ Індивідуальна робота №4/Індивідуальна робота №4.cpp
@@ -0,0 +1,38 @@
+﻿#include "iostream"
+using namespace std;
+
+
+int nod(int n, int m)
+{
+    if (m == 0)
+        return n;
+    if (n > m)
+        return nod(m, n - m);
+    else
+        return nod(n, m - n);
+}
+
+int main()
+{
+    int n = 4;
+    int* arr;
+    int nod_;
+    arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Enter " << i + 1 << " number " << endl;
+        cin >> arr[i];
+    }
+    if (n >= 2) {
+        nod_ = nod(arr[0], arr[1]);
+    }
+    else {
+        nod_ = arr[0];
+    }
+    for (int i = 2; i < n; i++) {
+        nod_ = nod(nod_, arr[i]);
+    }
+    cout << "NCD: " << nod_ << endl;
+    cout << "Variant: 143 "  << endl;
+    system("pause");
+    return 0;
+}
---
+++ Індивідуальна робота №5/Індивідуальна робота №5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char n[9];
+	int p[9];
+	for (int i = 0; i < 9; i++)
+		cin >> n[i];
+	for (int i = 0; i < 9; i++)
+	{
+		p[i] = n[i] - '0';
+	}
+	cout << "Digits in int (Variant 180): " << endl;
+	for (int i = 0; i < 9; i++)
+	{
+		cout << p[i];
+	}
+}
